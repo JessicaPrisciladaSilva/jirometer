053978560a1495f4efc646c7f5570d6d49dad968
Jessica <jessica.priscila@nubank.com.br>
2020-07-09 01:34:26 -0300

    Delete unused files

--- resources/public/cljs/out/jirometer/core.cljs
+++
@@ -1,4 +0,0 @@
-(ns jirometer.core)
-
-(let [el (.getElementById js/document "content")]
-  (set! (.-innerText el) "Hello World"))
--- resources/public/cljs/out/jirometer/core.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.10.764 {}
-goog.provide('jirometer.core');
-goog.require('cljs.core');
-var el_25961 = document.getElementById("content");
-(el_25961.innerText = "Hello World");
-
-//# sourceMappingURL=core.js.map?rel=1594268122001
