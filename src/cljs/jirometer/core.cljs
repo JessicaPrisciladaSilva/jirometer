6aad5ecf8b02022a4f4c354d833f416371289d18
Jessica <jessica.priscila@nubank.com.br>
2020-07-12 21:46:34 -0300

    Add new functions to read the fake issue and show in the page

--- src/cljs/jirometer/core.cljs
+++ src/cljs/jirometer/core.cljs
@@ -1,4 +1,31 @@
-(ns jirometer.core)
+(ns jirometer.core
+  (:require [rum.core :refer [defc mount]]))
 
-(let [el (.getElementById js/document "content")]
-  (set! (.-innerText el) "Hello World"))
+(defc heading []
+  [:h1 "JIRA Activity Feed"])
+
+(defc issue
+  [{:keys [id summary type timestamp]}]
+  [:div.issue
+   [:div.id id]
+   [:div.info
+    [:div.title summary]
+    [:div.detail type]
+    [:div.date timestamp]]])
+
+(defc issues
+  [is]
+  [:div.issues
+   (map issue is)])
+
+(def fake-issues '({:id "FOO-1" :summary "The Title" :type "jira:issue_updated" :timestamp 123456789}))
+
+(defc content
+  [data]
+  [:div.container
+   (heading)
+   (if data
+     (issues data)
+     [:div "No activity found in feed."])])
+
+(mount (content fake-issues) (.getElementById js/document "content"))
